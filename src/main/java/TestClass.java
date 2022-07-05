f1b0324ded4818fcbaee6cebf7f72a062de4c521
Vlad <stormilolz@gmail.com>
2022-07-06 01:29:43 +0300

    random method + call it from in "if" statement

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -3,12 +3,18 @@ import java.util.Random;
 public class TestClass {
 
     public static void main(String[] args) {
-        Random random = new Random();
-        int a = random.nextInt(6);
-        if (a == 5) {
+        if (random(6) == 5) {
             System.out.println("we got 5!");
         } else {
             System.out.println("we got something else, but '5' D:");
         }
+
+    }
+
+    public static int random (int bound) {
+        int result = 0;
+        Random random = new Random();
+        result = random.nextInt(bound);
+        return result;
     }
 }
